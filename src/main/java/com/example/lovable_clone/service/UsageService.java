fe278af7b07ec0716f090ebd8f92c77d01eb5e51
Sahil Rao <88286056+Sahilrao09@users.noreply.github.com>
2026-06-20 17:35:50 +0530

    Add controllers DTos and records

--- src/main/java/com/example/lovable_clone/service/UsageService.java
+++ src/main/java/com/example/lovable_clone/service/UsageService.java
@@ -1,5 +1,6 @@
 package com.example.lovable_clone.service;
 
+import com.example.lovable_clone.dto.subscription.PlanLimitsResponse;
 import com.example.lovable_clone.dto.subscription.UsageTodayResponse;
 
 public interface UsageService {
